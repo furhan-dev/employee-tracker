322415acaf2b66b5a09954baa2c57a59d7734435
Furhan Samani <contact@furhan.dev>
2022-07-15 10:53:15 -0700

    complete schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
+USE employee_db;
+
+# Create Tables
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT DEFAULT NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
