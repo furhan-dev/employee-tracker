800120687ea9ae66d9d5f33692eaaaa930c147c7
Furhan Samani <contact@furhan.dev>
2022-07-19 13:51:18 -0700

    addEmployee() with helper functions

--- server.js
+++ server.js
@@ -115,22 +115,23 @@ function displayEmployees() {
 }
 
 function addDepartment() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'dept',
-            message: "Enter new department name: ",
-            validate: dept => {
-                if (!dept) {
-                    // TODO: check against existing depts
-                    console.log('Please enter a valid department name');
-                    return false;
-                } else {
-                    return true;
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'dept',
+                message: "Enter new department name: ",
+                validate: dept => {
+                    if (!dept) {
+                        // TODO: check against existing depts
+                        console.log('Please enter a valid department name');
+                        return false;
+                    } else {
+                        return true;
+                    }
                 }
             }
-        }
-    ])
+        ])
         .then(answer => {
             const sql = `INSERT INTO departments (name)
                       VALUES ('${answer.dept}')`;
@@ -144,51 +145,92 @@ function addDepartment() {
 }
 
 function addRole() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: "Enter new role name: ",
-            validate: title => {
-                if (!title) {
-                    // TODO: check against existing roles
-                    console.log('Please enter a valid role name');
-                    return false;
-                } else {
-                    return true;
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: "Enter new role name: ",
+                validate: title => {
+                    if (!title) {
+                        // TODO: check against existing roles
+                        console.log('Please enter a valid role name');
+                        return false;
+                    } else {
+                        return true;
+                    }
                 }
-            }
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: "Enter the salary for the new role: ",
-            validate: salary => {
-                if (!salary || isNaN(salary)) {
-                    console.log('Please enter a valid salary');
-                    return false;
-                } else {
-                    return true;
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: "Enter the salary for the new role: ",
+                validate: salary => {
+                    if (!salary || isNaN(salary)) {
+                        console.log('Please enter a valid salary');
+                        return false;
+                    } else {
+                        return true;
+                    }
                 }
+            },
+            {
+                type: 'list',
+                name: 'dept',
+                message: "Which department does this role belong to?: ",
+                choices: getDeptList
             }
-        },
-        {
-            type: 'list',
-            name: 'dept',
-            message: "Which department does this role belong to?: ",
-            choices: getDeptList
-        }
-    ])
+        ])
         .then(async answer => {
-            const params = [answer.title, answer.salary, await getDeptId(answer.dept)]
+            const params = [answer.title, answer.salary, await getDeptId(answer.dept)];
             console.log(params);
             const sql = `INSERT INTO roles (title, salary, department_id)
-                      VALUES (?, ?, ?)`;
+                    VALUES (?, ?, ?)`;
             db.query(sql, params, (err, result) => {
                 if (err || !result) throw err;
 
                 console.log(`\nSuccessfully added ${answer.title}!`);
                 displayRoles();
+            });
+        });
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: "Enter employee's first name: ",
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "Enter employees last name: ",
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "Select the employee's role: ",
+                choices: getRoleList
+            },
+            {
+                type: 'list',
+                name: 'manager',
+                message: "Select the employee's manager: ",
+                choices: getManagerList
+            }
+        ])
+        .then(async answer => {
+            const params = [answer.firstName, answer.lastName, await getRoleId(answer.role), await getEmployeeId(answer.manager)];
+            console.log(params);
+            const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+                    VALUES (?, ?, ?, ?)`;
+            db.query(sql, params, (err, result) => {
+                if (err || !result) throw err;
+
+                console.log(`\nSuccessfully added ${answer.first_name} ${answer.last_name}!`);
+                displayEmployees();
             });
         });
 }
@@ -207,6 +249,54 @@ function getDeptId(name) {
     const sql = `SELECT id
                 FROM departments
                 WHERE departments.name = '${name}'`;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, rows) => {
+            if (err || !rows) reject(err);
+            console.log(rows[0].id);
+            resolve(rows[0].id);
+        });
+    });
+}
+
+function getRoleList() {
+    const sql = "SELECT title FROM roles";
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows.map(row => row.title));
+        });
+    });
+}
+
+function getRoleId(title) {
+    const sql = `SELECT id
+                FROM roles
+                WHERE roles.title= '${title}'`;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, rows) => {
+            if (err || !rows) reject(err);
+            console.log(rows[0].id);
+            resolve(rows[0].id);
+        });
+    });
+}
+
+function getManagerList() {
+    const sql = "SELECT first_name, last_name FROM employees WHERE role_id=1";
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows.map(row => row.first_name + " " + row.last_name));
+        });
+    });
+}
+
+function getEmployeeId(fullname) {
+    const splitName = fullname.split(' ');
+    const sql = `SELECT id
+                FROM employees
+                WHERE employees.first_name = '${splitName[0]}'
+                    AND employees.last_name = '${splitName[1]}'`;
     return new Promise((resolve, reject) => {
         db.query(sql, (err, rows) => {
             if (err || !rows) reject(err);
